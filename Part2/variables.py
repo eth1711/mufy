519e6d7dc90b5fe25f7d268bd088922245a3467e
Ethan Lim <111282806+eth1711@users.noreply.github.com>
2026-05-25 05:23:50 +0000

    done

---
+++ Part2/variables.py
@@ -0,0 +1,4 @@
+
+variable_1 = 2
+Variable_2 = 1
+
---
+++ lists.py
@@ -0,0 +1,9 @@
+# List of names
+names = ['Tom', 'Jerry', 'Butch', 'Tuffy', 'Pecos']
+
+# TODO: Try the following:
+# Print the whole list
+# Print the first name 
+# Print the last name
+# Print the middle two names using list slicing
+print(names[1:3])
